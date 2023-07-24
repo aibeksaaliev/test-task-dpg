d04c7f386044ad2558e202a087bd948b3e3ddeb6
Aibek_Saaliev <dokaidea@gmail.com>
2023-07-24 15:07:33 +0600

    #3 - Created preliminary component for displaying all graph days.

--- src/components/Graphs/ContributionGraph.tsx
+++ src/components/Graphs/ContributionGraph.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import "../../styles/contribution-graph.css";
 import CellDay from "./CellDay";
 
 const ContributionGraph = () => {
+    const generate365 = () => {
+        const days = [];
+        const today = new Date();
+        let yearBefore = new Date(today.getTime());
+        yearBefore.setDate(today.getDate() - 357);
+
+        for (let i = 0; i < 357; i++) {
+            let generatedDay = new Date(yearBefore);
+            generatedDay.setDate(yearBefore.getDate() + i);
+            days.push(generatedDay);
+        }
+
+        return days;
+    };
+
     return (
-        <div>
-            <CellDay/>
+        <div className="contribution-graph">
+            {generate365().map((day) => {
+                return <CellDay key={day.toISOString()} day={day.toISOString()}/>;
+            })}
         </div>
     );
 };
