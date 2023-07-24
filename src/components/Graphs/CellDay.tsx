eddfba82a30e261b5fc6142f44a7af3e5060ba51
Aibek_Saaliev <dokaidea@gmail.com>
2023-07-24 17:05:40 +0600

    #8 - Added classes for displaying different types of cells, added popup with displaying contribution info.

--- src/components/Graphs/CellDay.tsx
+++ src/components/Graphs/CellDay.tsx
@@ -1,13 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "../../styles/cell-day.css";
+import {ContributionCell} from "../../types";
+import dayjs from 'dayjs';
+import 'dayjs/locale/ru';
 
 interface Props {
-    day: string;
+    day: ContributionCell;
 }
 
 const CellDay: React.FC<Props> = ({day}) => {
+    const [popup, setPopup] = useState<boolean>(false);
+    const detectContributionCellColor = (day: ContributionCell) => {
+        if (day.amount >= 1 && day.amount <= 9) {
+            return 'light-blue';
+        } else if (day.amount >= 10 && day.amount <= 19) {
+            return 'blue';
+        } else if (day.amount >= 20 && day.amount <= 29) {
+            return 'dark-blue';
+        } else if (day.amount >= 30) {
+            return 'deep-blue';
+        } else {
+            return "";
+        }
+    };
+
+    const formatDateToRussian = (inputDate: string) => {
+        const date = dayjs(inputDate);
+        return date.locale('ru').format('dddd, MMMM DD, YYYY');
+    };
+
+    const inputDate = day.date;
+    const formattedDate = formatDateToRussian(inputDate);
+
+    const handleMouseEnter = () => {
+        setPopup(true);
+    };
+
+    const handleMouseLeave = () => {
+        setPopup(false);
+    };
+
+    const classesList = "cell-day " + detectContributionCellColor(day);
+
     return (
-        <div className="cell-day">
+        <div
+            className={classesList}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+            {popup && (
+                <div className="popup">
+                    <div className="popup-info">
+                        <p className="amount-info">{day.amount} contributions</p>
+                        <span className="date-info">{formattedDate}</span>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
