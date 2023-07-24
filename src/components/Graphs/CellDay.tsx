66393c8201e38559234941115142fe59b7246bf3
Aibek_Saaliev <dokaidea@gmail.com>
2023-07-24 14:42:30 +0600

    #1 - Removed unused files, created preliminary component for cell day.

---
+++ src/components/Graphs/CellDay.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../../styles/cell-day.css";
+
+const CellDay = () => {
+    return (
+        <div className="cell-day">
+
+        </div>
+    );
+};
+
+export default CellDay;
---
+++ src/components/Graphs/ContributionGraph.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CellDay from "./CellDay";
+
+const ContributionGraph = () => {
+    return (
+        <div>
+            <CellDay/>
+        </div>
+    );
+};
+
+export default ContributionGraph;
